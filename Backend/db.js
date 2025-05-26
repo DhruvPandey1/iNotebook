3e69acf34c06658ab475b249734384a8b890be84
DhruvPandey1 <115931661+DhruvPandey1@users.noreply.github.com>
2025-05-27 00:36:43 +0530

    Backend

--- Backend/db.js
+++ Backend/db.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
-const mongoURI="mongodb://127.0.0.1:27017/inotebook?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.9.1"
-const connectToMongo=()=>{mongoose.connect(mongoURI)};
+require('dotenv').config();
+connectToMongo=()=>{mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.log(err));
+}
 module.exports=connectToMongo;
